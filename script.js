04579b4f2f3964368f0d8f8286e6994b32e37fe3
JarekSmith <105569335+JarekSmith@users.noreply.github.com>
2026-04-26 22:53:45 -0600

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasElement = document.getElementById("canvas-element");
+    const context = canvasElement.getContext("2d");
+    
+    const ratio = getRatio();
+    context.fillStyle = "blue";
+    context.fillRect(0, 0, 1000 * ratio, 90.8);
+
+    const currentTime = new Date();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    document.getElementsByClassName("timestamp")[0].innerText = `${months[currentTime.getMonth()]} ${currentTime.getDate()}, ${currentTime.getFullYear()}`;
+})
+
+const getRatio = () => {
+    const first = new Date(2026, 0, 18).valueOf();
+    const last = new Date(2027, 6, 2).valueOf();
+    const current = new Date().valueOf();
+    return (current - first) / (last - first);
+}
